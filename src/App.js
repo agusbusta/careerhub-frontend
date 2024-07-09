9e252d6488cf36876ae3034819ca3790e8597290
Agustin Bustamante <agustinbustamante@Agustins-MBP-2.fibertel.com.ar>
2024-07-09 13:44:34 -0300

    foldered some files

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
 import Dashboard from './pages/Dashboard';
 import '../src/styles.css';
 
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import config from '../config';
+import config from '../../config';
 import './Login.css'
 
 function Login() {
